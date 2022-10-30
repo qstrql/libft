ab849c8e4a3580287c10e56f730914cc9b1194ad
qstrql <mjouot@student.42angouleme.fr>
2022-10-30 23:28:23 +0100

    printf tab + updated written message

--- SRCS/extra/ft_printf_tab.c
+++ SRCS/extra/ft_printf_tab.c
@@ -1,26 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf_strs.c                                   :+:      :+:    :+:   */
+/*   ft_printf_tab.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mjouot <mjouot@marvin.42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/30 16:23:50 by mjouot            #+#    #+#             */
-/*   Updated: 2022/10/30 23:27:09 by mjouot           ###   ########.fr       */
+/*   Created: 2022/10/30 23:24:28 by mjouot            #+#    #+#             */
+/*   Updated: 2022/10/30 23:27:27 by mjouot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../INCLUDES/ft_printf.h"
 #include "../../INCLUDES/libft.h"
 
-void	ft_printf_strs(char **strs)
+void	ft_printf_tab(int *tab, int len)
 {
 	int	i;
 
 	i = 0;
-	while (strs[i] != NULL)
+	while (i < len)
 	{
-		ft_printf("strs[%d] : %s\n", i, strs[i]);
+		ft_printf("tab[%d] : %d\n", i, tab[i]);
 		i++;
 	}
 }
